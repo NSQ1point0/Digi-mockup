cdf4a7191c7d96305573707f165e7097cb93d446
NumbSkullq1 <Wolfstreetmack@gmail.com>
2018-04-05 23:54:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+$("#lang").click(function() {
+    document.getElementById("dropdownLang").classList.toggle("show");
+    });
+
+    $("#loc-btn").click(function() {
+        document.getElementById("dropdownLoc").classList.toggle("show");
+        });
+
+        $(".hotelRoomA").hover(function(){
+    $('.hotel-btna').show();
+},function(){
+    $('.hotel-btna').hide();
+});
+
+$(".hotelRoomB").hover(function(){
+$('.hotel-btnb').show();
+},function(){
+$('.hotel-btnb').hide();
+});
+
+$(".hotelRoomC").hover(function(){
+$('.hotel-btnc').show();
+},function(){
+$('.hotel-btnc').hide();
+});
+
+$(".hotelRoomD").hover(function(){
+$('.hotel-btnd').show();
+},function(){
+$('.hotel-btnd').hide();
+});
+
+$(".hotelRoomE").hover(function(){
+$('.hotel-btne').show();
+},function(){
+$('.hotel-btne').hide();
+});
